278f735770050261aca5cba1f12898d3e84a26e5
Harkirat singh <harkiratsingh708k@gmail.com>
2024-07-28 09:14:35 -0400

    Darkening and lightening effect added

--- Assignment4/assignmentpart2.js
+++ Assignment4/assignmentpart2.js
@@ -26,6 +26,16 @@ for (const picture of pictures){
     });
 }
 
-
-
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function(){
+    if(btn.getAttribute('class') == 'dark'){
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    }
+    else{
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+})

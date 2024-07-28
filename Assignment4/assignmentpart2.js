c23ec7c3e91da61661dfec9450c5cfbe42eacbbd
Harkirat singh <harkiratsingh708k@gmail.com>
2024-07-28 08:55:23 -0400

    Created an Array and a Object

--- Assignment4/assignmentpart2.js
+++ Assignment4/assignmentpart2.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const pictures = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {'pic.jpg' : 'Closeup Shot of Eye', 'pic2.jpg' : 'Waves', 'pic3.jpg' : 'Blue Flowers', 'pic4.jpg' : 'Ancient Drawing', 'pic5.jpg' : 'Butterfly'};
 
 /* Looping through images */
 
